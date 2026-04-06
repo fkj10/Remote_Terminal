0d7095f1d8ceb80648795fb5718b81ffcf0c9837
fkj10 <102484997+fkj10@users.noreply.github.com>
2026-04-06 16:54:12 +0800

    添加部分注释,便于配置

--- config.py
+++ config.py
@@ -5,14 +5,14 @@ from openai import OpenAI
 class Config:
     def __init__(self):
         # Global
-        self.group_id = int # 群号
-        self.ws_url=""
-        self.token=""
+        self.group_id = int # 输入你的群号，类型为int
+        self.ws_url="" # 输入你的ws地址，格式为ws://ip:port
+        self.token="" # 输入你的token，格式为字符串, 即前文"配置napcat"中的token
         self.support_name = [""] # 配置ai所支持的呼出名称
 
 
         #AI config
-        self.github_PAT = "" #请勿轻易修改，此为api的访问令牌 
+        self.github_PAT = "" #请勿轻易修改，此为api的访问令牌,输入你的github_PAT，格式为字符串
 
         self.reply_threshold = 5 # 回复频率限制，单位为次/分钟，防止过于频繁的回复导致被封禁
         # 提示词
@@ -31,7 +31,7 @@ class Config:
         # OpenAI client 配置 
 
         #auto_kick
-        self.dictionary = "bad_words.txt"
+        self.dictionary = "bad_words.txt" # 输入你的敏感词文件路径，格式为字符串 , 且每个敏感词使用空格分隔开
         
         #no_spamming
         self.spam_threshold = 5
